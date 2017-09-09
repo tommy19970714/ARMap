6aa854a051055b52d6d7acfcf97dba00e4027d98
tommy19970714 <toshiki0714@icloud.com>
2017-09-09 14:10:49 +0900

    initial commit

---
+++ ARMap/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ARMap
+//
+//  Created by 冨平準喜 on 2017/09/08.
+//  Copyright © 2017年 冨平準喜. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
---
+++ Pods/Target Support Files/Pods-ARMapTests/Pods-ARMapTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ARMapTests : NSObject
+@end
+@implementation PodsDummy_Pods_ARMapTests
+@end
